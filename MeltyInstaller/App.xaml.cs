6e5e73c2712837a1415f69db53d70faab3c52080
okkdev <dev@stehlik.me>
2021-07-31 00:53:48 +0200

    initial commit

---
+++ MeltyInstaller/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using MeltyInstaller.Core;
+
+namespace MeltyInstaller
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.Activated += StartElmish;
+        }
+
+        private void StartElmish(object sender, EventArgs e)
+        {
+            this.Activated -= StartElmish;
+            Program.main(MainWindow);
+        }
+    }
+}
